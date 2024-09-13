3e8da39a25c0cb9c13fd77df6d85884639faedd6
ilyainavoid <thirdilno@gmail.com>
2024-09-13 16:29:47 +0700

    ☄️ feat: move query creating logic to utils

--- src/data/requests/user/get-users.ts
+++ src/data/requests/user/get-users.ts
@@ -1,20 +1,10 @@
 import { AxiosRequestConfig, GetUsersResponse, SearchParams } from '@/types/api'
 import api from '@/data/requests/axios-instance.ts'
+import { createQueryParams } from '@/lib/utils.ts'
 
 export type GetUsersConfig = AxiosRequestConfig<SearchParams>
 
 export const getUsers = ({ params, config }: GetUsersConfig) => {
-  const queryParams = new URLSearchParams()
-
-  if (params) {
-    if (params.search) queryParams.append('search', params.search)
-    if (params.page) queryParams.append('page', params.page.toString())
-    if (params.limit) queryParams.append('limit', params.limit.toString())
-    if (params.sortBy) queryParams.append('sortBy', params.sortBy.join(','))
-    if (params.sortOrder) queryParams.append('sortOrder', params.sortOrder)
-  }
-
-  const url = `/users${queryParams.toString() ? `?${queryParams.toString()}` : ''}`
-
+  const url = `/users${createQueryParams(params)}`
   return api.get<GetUsersResponse>(url, config)
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { SearchParams } from '@/types/api'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const createQueryParams = (params?: SearchParams): string => {
+  const queryParams = new URLSearchParams()
+
+  if (params) {
+    if (params.search) queryParams.append('search', params.search)
+    if (params.page) queryParams.append('page', params.page.toString())
+    if (params.limit) queryParams.append('limit', params.limit.toString())
+    if (params.sortBy) queryParams.append('sortBy', params.sortBy.join(','))
+    if (params.sortOrder) queryParams.append('sortOrder', params.sortOrder)
+  }
+
+  return queryParams.toString() ? `?${queryParams.toString()}` : ''
 }
