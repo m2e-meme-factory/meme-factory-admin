d01a3b91e38dd811659e0bb95f9818eac2969962
ilyainavoid <thirdilno@gmail.com>
2024-09-19 20:20:38 +0700

    ⚡️ fix: wrong import

--- src/data/requests/auto-task/create-autotask.ts
+++ src/data/requests/auto-task/create-autotask.ts
@@ -1,6 +1,7 @@
-import { AutotaskDto, AxiosRequestConfig, CreateAutoTaskDto } from '@/types/api'
+import { AxiosRequestConfig, CreateAutoTaskDto } from '@/types/api'
 import api from '@/data/requests/axios-instance.ts'
+import { Autotask } from '@/pages/autotasks/data/schema.ts'
 
 export type CreateAutotaskConfig = AxiosRequestConfig<CreateAutoTaskDto>
 export const createAutotask = ({ params, config }: CreateAutotaskConfig) =>
-  api.post<AutotaskDto>('/auto-task', params, config)
+  api.post<Autotask>('/auto-task', params, config)
