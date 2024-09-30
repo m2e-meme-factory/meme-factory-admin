d9c43785170e3cc66b2b082a0907c1824c05fa2f
ilyainavoid <thirdilno@gmail.com>
2024-09-30 14:32:01 +0700

    ⚡️ fix: vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['quill'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
 })
